4e6f60afa1df769fbdb6c20acc0457113c213c52
FullStackCodeJones <fullstackcodejones@gmail.com>
2024-11-09 16:57:24 -0500

    Made an entire webpage... HATED IT

--- scipts.js
+++ scipts.js
@@ -1,19 +1,18 @@
-//Adding some dynamic interactivity (just in case I want to spice it up later!)
+/*document.addEventListener("DOMContentLoaded", () => {
+  const form = document.getElementById("programForm");
 
-// Simple function to change the background color when a button is clicked
-// For now, this is just a placeholder in case I want to experiment later!
+  form.addEventListener("submit", function (event) {
+    event.preventDefault(); // Prevents the page from refreshing
 
-document.addEventListener("DOMContentLoaded", () => {
-  // This would be for a future interactive element.
-  // If I had a button, I could easily add an event listener to it like this.
+    const selectedProgram = document.getElementById("program").value;
 
-  console.log(
-    "Sissies House is ready! Time to change the World or at least help make it better!"
-  );
-
-  //Adding an event listener if I do decide to make things interactive:
-  // const button = document.querySelector('button');
-  // button.addEventListener('click', () => {
-  //   document.body.style.backgroundColor = 'lightblue'; // Changes the background color
-  // });
-});
+    // Simple validation: Check if the user has selected a program
+    if (!selectedProgram) {
+      alert("Please select a program to get involved.");
+    } else {
+      alert(
+        `You have chosen to get involved in the ${selectedProgram} program.`
+      );
+    }
+  }); 
+});*/
