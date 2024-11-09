00d649a49ee3862b5e274e267f30453d98f5c604
FullStackCodeJones <fullstackcodejones@gmail.com>
2024-11-09 15:39:42 -0500

    I don't know what happened before, but putting this here for a placeholder to add on when I start my portfolio

---
+++ scipts.js
@@ -0,0 +1,19 @@
+//Adding some dynamic interactivity (just in case I want to spice it up later!)
+
+// Simple function to change the background color when a button is clicked
+// For now, this is just a placeholder in case I want to experiment later!
+
+document.addEventListener("DOMContentLoaded", () => {
+  // This would be for a future interactive element.
+  // If I had a button, I could easily add an event listener to it like this.
+
+  console.log(
+    "Sissies House is ready! Time to change the World or at least help make it better!"
+  );
+
+  //Adding an event listener if I do decide to make things interactive:
+  // const button = document.querySelector('button');
+  // button.addEventListener('click', () => {
+  //   document.body.style.backgroundColor = 'lightblue'; // Changes the background color
+  // });
+});
